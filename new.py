771853c1280ffac4cbee047f23c1f7e1396864d8
Nishant1121 <nishantaiku1121@gmail.com>
2026-07-19 23:41:43 +0530

    Just Python Syntax

---
+++ new.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+
+
+# Python Indentation
+# Indentation refers to the spaces at the beginning of a code line.
+if 5 > 2:
+  print("Five is greater than two!")
+
+#if 5 > 2:
+#print("Five is greater than two!") error, because the print statement is not indented
